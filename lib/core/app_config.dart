469721bd6546ac2dda7df1c705f508a080953ea8
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-05-06 20:22:41 -0300

    feat: adicionar configurador de url base para requisições a partir da plataforma

---
+++ lib/core/app_config.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class AppConfig {
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    }
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    }
+    return 'http://localhost:8080';
+  }
+}
