d99147f3c3038e97c8cada877838d6f3a8930c2b
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-05-06 20:45:10 -0300

    fix: modificar nome do app na classe de testes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:viagens_mobile/main.dart';
+import 'package:viagens_mobile/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ViagensApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
