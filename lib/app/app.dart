4dc8ab38d3ea6241b4dd110523493dd269dec557
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-05-06 21:46:39 -0300

    feat: atualizar instâncias que serão gerenciadas pelo provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,7 +6,18 @@ import 'package:viagens_mobile/app/theme.dart';
 import 'package:viagens_mobile/core/app_config.dart';
 import 'package:viagens_mobile/data/api/api_client.dart';
 import 'package:viagens_mobile/data/auth/auth_api.dart';
+import 'package:viagens_mobile/data/domains/domains_api.dart';
+import 'package:viagens_mobile/data/repositories/auth_repository_impl.dart';
+import 'package:viagens_mobile/data/repositories/domains_repository_impl.dart';
+import 'package:viagens_mobile/data/repositories/trips_repository_impl.dart';
 import 'package:viagens_mobile/data/storage/token_storage.dart';
+import 'package:viagens_mobile/data/trips/trips_api.dart';
+import 'package:viagens_mobile/domain/contracts/auth_repository.dart';
+import 'package:viagens_mobile/domain/contracts/domains_repository.dart';
+import 'package:viagens_mobile/domain/contracts/trips_repository.dart';
+import 'package:viagens_mobile/features/auth/state/auth_controller.dart';
+import 'package:viagens_mobile/features/trips/state/domains_controller.dart';
+import 'package:viagens_mobile/features/trips/state/trips_controller.dart';
 
 class ViagensApp extends StatelessWidget {
   const ViagensApp({super.key});
@@ -23,7 +34,41 @@ class ViagensApp extends StatelessWidget {
           ),
         ),
         Provider<AuthApi>(create: (context) => AuthApi(context.read<ApiClient>())),
-        
+        Provider<TripsApi>(create: (context) => TripsApi(context.read<ApiClient>())),
+        Provider<DomainsApi>(
+          create: (context) => DomainsApi(context.read<ApiClient>()),
+        ),
+        Provider<IAuthRepository>(
+          create: (context) => AuthRepositoryImpl(
+            authApi: context.read<AuthApi>(),
+            tokenStorage: context.read<TokenStorage>(),
+          ),
+        ),
+        Provider<ITripsRepository>(
+          create: (context) => TripsRepositoryImpl(
+            tripsApi: context.read<TripsApi>(),
+          ),
+        ),
+        Provider<IDomainsRepository>(
+          create: (context) => DomainsRepositoryImpl(
+            domainsApi: context.read<DomainsApi>(),
+          ),
+        ),
+        ChangeNotifierProvider<AuthController>(
+          create: (context) => AuthController(
+            authRepository: context.read<IAuthRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<TripsController>(
+          create: (context) => TripsController(
+            tripsRepository: context.read<ITripsRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<DomainsController>(
+          create: (context) => DomainsController(
+            domainsRepository: context.read<IDomainsRepository>(),
+          ),
+        ),
       ],
       child: MaterialApp(
         title: 'Viagens',
