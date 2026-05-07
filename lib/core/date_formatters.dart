e77e54699f8bf6536f63349c26d74315746e96b6
MuriloVantini <m.henrique.vantini18@gmail.com>
2026-05-06 21:47:31 -0300

    refactor: segregar função de formatação de data de forma que fique no bloco de core

--- lib/core/date_formatters.dart
+++ lib/core/date_formatters.dart
@@ -1,3 +1,10 @@
 import 'package:intl/intl.dart';
 
 final DateFormat shortDate = DateFormat('dd/MM/yyyy');
+
+String formatDate(DateTime date) {
+    final year = date.year.toString().padLeft(4, '0');
+    final month = date.month.toString().padLeft(2, '0');
+    final day = date.day.toString().padLeft(2, '0');
+    return '$year-$month-$day';
+  }
